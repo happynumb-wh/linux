jeevandeepj994/anolis_perf

anolis: blk: add iohang check function
ANBZ: #8419

Background:
  We do not have a dependable block layer interface to determine whether
block device has io requests which have not been completed for somewhat
long time. Currently we have 'in_flight' interface, it counts the number
of I/O requests that have been issued to the device driver but have
not yet completed, and it does not include I/O requests that are in the
queue but not yet issued to the device driver, which means it will not
count io requests that have been stucked in block layer.
  Also say that there are steady io requests issued to device driver,
'in_flight' maybe always non-zero, but you could not determine whether
there is one io request which has not been completed for too long.

Solution:
  To find io requests which have not been completed for too long, here
add 3 new inferfaces:
  /sys/block/vdb/queue/hang_threshold
If one io request's running time has been greater than this value, count
this io as hang.

  /sys/block/vdb/hang
Show read/write io requests' hang counter.

  /sys/kernel/debug/block/vdb/rq_hang
Show all hang io requests's detailed info, like below:
  ffff97db96301200 {.op=WRITE, .cmd_flags=SYNC, .rq_flags=STARTED|
ELVPRIV|IO_STAT|STATS, .state=in_flight, .tag=30, .internal_tag=169,
.start_time_ns=140634088407, .io_start_time_ns=140634102958,
.current_time=146497371953, .bio = ffff97db91e8e000,
.bio_pages = { ffffd096a0602540 }, .bio = ffff97db91e8ec00,
.bio_pages = { ffffd096a070eec0 }, .bio = ffff97db91e8f600,
.bio_pages = { ffffd096a0424cc0 }, .bio = ffff97db91e8f300,
.bio_pages = { ffffd096a0600a80 }}

With above info, we can easily see this request's latency distribution,
and see next patch for bio_pages's usage.

Note this feature needs CONFIG_BLK_DEBUG_FS to be enabled.

Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

block/blk-mq-debugfs.c
@@ -155,12 +155,47 @@ inval:
 	return count;
 }
 
+static void blk_mq_debugfs_rq_hang_show(struct seq_file *m, struct request *rq);
+
+static bool blk_mq_check_rq_hang(struct request *rq, void *priv)
+{
+	struct seq_file *m = priv;
+	u64 now = ktime_get_ns();
+	u64 duration;
+
+	duration = div_u64(now - rq->start_time_ns, NSEC_PER_MSEC);
+	if (duration < rq->q->rq_hang_threshold)
+		return true;
+
+	/* See comments in blk_mq_check_expired() */
+	if (!req_ref_inc_not_zero(rq))
+		return true;
+
+	duration = div_u64(now - rq->start_time_ns, NSEC_PER_MSEC);
+	if (duration >= rq->q->rq_hang_threshold)
+		blk_mq_debugfs_rq_hang_show(m, rq);
+
+	blk_mq_put_rq_ref(rq);
+
+	return true;
+
+}
+
+static int queue_rq_hang_show(void *data, struct seq_file *m)
+{
+	struct request_queue *q = data;
+
+	blk_mq_queue_tag_busy_iter(q, blk_mq_check_rq_hang, m);
+	return 0;
+}
+
 static const struct blk_mq_debugfs_attr blk_mq_debugfs_queue_attrs[] = {
 	{ "poll_stat", 0400, queue_poll_stat_show },
 	{ "requeue_list", 0400, .seq_ops = &queue_requeue_list_seq_ops },
 	{ "pm_only", 0600, queue_pm_only_show, NULL },
 	{ "state", 0600, queue_state_show, queue_state_write },
 	{ "zone_wlock", 0400, queue_zone_wlock_show, NULL },
+	{ "rq_hang", 0400, queue_rq_hang_show, NULL },
 	{ },
 };
 
@@ -309,6 +344,52 @@ int blk_mq_debugfs_rq_show(struct seq_file *m, void *v)
 	return __blk_mq_debugfs_rq_show(m, list_entry_rq(v));
 }
 EXPORT_SYMBOL_GPL(blk_mq_debugfs_rq_show);
+
+static void blk_mq_debugfs_rq_hang_show(struct seq_file *m, struct request *rq)
+{
+	const struct blk_mq_ops *const mq_ops = rq->q->mq_ops;
+	const unsigned int op = req_op(rq);
+	const char *op_str = blk_op_str(op);
+	struct bio *bio;
+	struct bio_vec *bvec;
+	struct bvec_iter_all iter_all;
+
+	seq_printf(m, "%p {.op=", rq);
+	if (strcmp(op_str, "UNKNOWN") == 0)
+		seq_printf(m, "%u", op);
+	else
+		seq_printf(m, "%s", op_str);
+	seq_puts(m, ", .cmd_flags=");
+	blk_flags_show(m, rq->cmd_flags & ~REQ_OP_MASK, cmd_flag_name,
+		       ARRAY_SIZE(cmd_flag_name));
+	seq_puts(m, ", .rq_flags=");
+	blk_flags_show(m, (__force unsigned int)rq->rq_flags, rqf_name,
+		       ARRAY_SIZE(rqf_name));
+	seq_printf(m, ", .state=%s", blk_mq_rq_state_name(blk_mq_rq_state(rq)));
+	seq_printf(m, ", .tag=%d, .internal_tag=%d", rq->tag,
+		   rq->internal_tag);
+	seq_printf(m, ", .start_time_ns=%llu", rq->start_time_ns);
+	seq_printf(m, ", .io_start_time_ns=%llu", rq->io_start_time_ns);
+	seq_printf(m, ", .current_time=%llu", ktime_get_ns());
+
+	__rq_for_each_bio(bio, rq) {
+		seq_printf(m, ", .bio = %px", bio);
+		seq_printf(m, ", .sector = %llu, .len=%u",
+			   bio->bi_iter.bi_sector, bio->bi_iter.bi_size);
+		seq_puts(m, ", .bio_pages = { ");
+		bio_for_each_segment_all(bvec, bio, iter_all) {
+			struct page *page = bvec->bv_page;
+
+			if (!page)
+				continue;
+			seq_printf(m, "%px ", page);
+		}
+		seq_puts(m, "}");
+	}
+	if (mq_ops->show_rq)
+		mq_ops->show_rq(m, rq);
+	seq_puts(m, "}\n");
+}
 
 static void *hctx_dispatch_start(struct seq_file *m, loff_t *pos)
 	__acquires(&hctx->lock)

block/blk-sysfs.c
@@ -438,6 +438,26 @@ static ssize_t queue_io_timeout_store(struct request_queue *q, const char *page,
 	return count;
 }
 
+static ssize_t queue_hang_threshold_show(struct request_queue *q, char *page)
+{
+	return sprintf(page, "%u\n", q->rq_hang_threshold);
+}
+
+static ssize_t queue_hang_threshold_store(struct request_queue *q, const char *page,
+				size_t count)
+{
+	unsigned int hang_threshold;
+	int err;
+
+	err = kstrtou32(page, 10, &hang_threshold);
+	if (err || hang_threshold == 0)
+		return -EINVAL;
+
+	blk_queue_rq_hang_threshold(q, hang_threshold);
+
+	return count;
+}
+
 static ssize_t queue_wc_show(struct request_queue *q, char *page)
 {
 	if (test_bit(QUEUE_FLAG_WC, &q->queue_flags))
@@ -527,6 +547,7 @@ QUEUE_RO_ENTRY(queue_dax, "dax");
 QUEUE_RW_ENTRY(queue_io_timeout, "io_timeout");
 QUEUE_RO_ENTRY(queue_virt_boundary_mask, "virt_boundary_mask");
 QUEUE_RO_ENTRY(queue_dma_alignment, "dma_alignment");
+QUEUE_RW_ENTRY(queue_hang_threshold, "hang_threshold");
 
 #ifdef CONFIG_BLK_DEV_THROTTLING_LOW
 QUEUE_RW_ENTRY(blk_throtl_sample_time, "throttle_sample_time");
@@ -656,6 +677,7 @@ static struct attribute *queue_attrs[] = {
 #endif
 	&queue_virt_boundary_mask_entry.attr,
 	&queue_dma_alignment_entry.attr,
+	&queue_hang_threshold_entry.attr,
 	NULL,
 };
 

block/genhd.c
@@ -1004,6 +1004,23 @@ ssize_t part_inflight_show(struct device *dev, struct device_attribute *attr,
 	return sprintf(buf, "%8u %8u\n", inflight[0], inflight[1]);
 }
 
+ssize_t part_hang_show(struct device *dev, struct device_attribute *attr,
+		       char *buf)
+{
+	struct block_device *bdev = dev_to_bdev(dev);
+	struct request_queue *q = bdev_get_queue(bdev);
+	unsigned int hang[2] = {0, 0};
+
+	/*
+	 * For now, we only support mq device, since don't find a generic method
+	 * to track reqs in single queue device.
+	 */
+	if (queue_is_mq(q))
+		blk_mq_hang_rw(q, bdev, hang);
+
+	return sprintf(buf, "%8u %8u\n", hang[0], hang[1]);
+}
+
 static ssize_t disk_capability_show(struct device *dev,
 				    struct device_attribute *attr, char *buf)
 {
@@ -1048,6 +1065,7 @@ static DEVICE_ATTR(discard_alignment, 0444, disk_discard_alignment_show, NULL);
 static DEVICE_ATTR(capability, 0444, disk_capability_show, NULL);
 static DEVICE_ATTR(stat, 0444, part_stat_show, NULL);
 static DEVICE_ATTR(inflight, 0444, part_inflight_show, NULL);
+static DEVICE_ATTR(hang, 0444, part_hang_show, NULL);
 static DEVICE_ATTR(badblocks, 0644, disk_badblocks_show, disk_badblocks_store);
 static DEVICE_ATTR(diskseq, 0444, diskseq_show, NULL);
 
@@ -1091,6 +1109,7 @@ static struct attribute *disk_attrs[] = {
 	&dev_attr_capability.attr,
 	&dev_attr_stat.attr,
 	&dev_attr_inflight.attr,
+	&dev_attr_hang.attr,
 	&dev_attr_badblocks.attr,
 	&dev_attr_events.attr,
 	&dev_attr_events_async.attr,

happynumb-wh/linux

cherry picked from devel-6.6 commit 5c3c218d8bb840261cebe0c6598853397e3c38d5.
ANBZ: #34068
[ remove ref get in blk_mq_check_rq_hang() since bi_iter() already does ]
Reviewed-by: Baokun Li <libaokun@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/6847

block/blk-mq-debugfs.c
@@ -148,12 +148,36 @@ inval:
 	return count;
 }
 
+static void blk_mq_debugfs_rq_hang_show(struct seq_file *m, struct request *rq);
+
+static bool blk_mq_check_rq_hang(struct request *rq, void *priv)
+{
+	struct seq_file *m = priv;
+	u64 now = ktime_get_ns();
+	u64 duration;
+
+	duration = div_u64(now - rq->start_time_ns, NSEC_PER_MSEC);
+	if (duration >= READ_ONCE(rq->q->rq_hang_threshold))
+		blk_mq_debugfs_rq_hang_show(m, rq);
+
+	return true;
+}
+
+static int queue_rq_hang_show(void *data, struct seq_file *m)
+{
+	struct request_queue *q = data;
+
+	blk_mq_queue_tag_busy_iter(q, blk_mq_check_rq_hang, m);
+	return 0;
+}
+
 static const struct blk_mq_debugfs_attr blk_mq_debugfs_queue_attrs[] = {
 	{ "poll_stat", 0400, queue_poll_stat_show },
 	{ "requeue_list", 0400, .seq_ops = &queue_requeue_list_seq_ops },
 	{ "pm_only", 0600, queue_pm_only_show, NULL },
 	{ "state", 0600, queue_state_show, queue_state_write },
 	{ "zone_wplugs", 0400, queue_zone_wplugs_show, NULL },
+	{ "rq_hang", 0400, queue_rq_hang_show, NULL },
 	{ },
 };
 
@@ -296,6 +320,52 @@ int blk_mq_debugfs_rq_show(struct seq_file *m, void *v)
 	return __blk_mq_debugfs_rq_show(m, list_entry_rq(v));
 }
 EXPORT_SYMBOL_GPL(blk_mq_debugfs_rq_show);
+
+static void blk_mq_debugfs_rq_hang_show(struct seq_file *m, struct request *rq)
+{
+	const struct blk_mq_ops *const mq_ops = rq->q->mq_ops;
+	const unsigned int op = req_op(rq);
+	const char *op_str = blk_op_str(op);
+	struct bio *bio;
+	struct bio_vec *bvec;
+	struct bvec_iter_all iter_all;
+
+	seq_printf(m, "%px {.op=", rq);
+	if (strcmp(op_str, "UNKNOWN") == 0)
+		seq_printf(m, "%u", op);
+	else
+		seq_printf(m, "%s", op_str);
+	seq_puts(m, ", .cmd_flags=");
+	blk_flags_show(m, rq->cmd_flags & ~REQ_OP_MASK, cmd_flag_name,
+		       ARRAY_SIZE(cmd_flag_name));
+	seq_puts(m, ", .rq_flags=");
+	blk_flags_show(m, (__force unsigned int)rq->rq_flags, rqf_name,
+		       ARRAY_SIZE(rqf_name));
+	seq_printf(m, ", .state=%s", blk_mq_rq_state_name(blk_mq_rq_state(rq)));
+	seq_printf(m, ", .tag=%d, .internal_tag=%d", rq->tag,
+		   rq->internal_tag);
+	seq_printf(m, ", .start_time_ns=%llu", rq->start_time_ns);
+	seq_printf(m, ", .io_start_time_ns=%llu", rq->io_start_time_ns);
+	seq_printf(m, ", .current_time=%llu", ktime_get_ns());
+
+	__rq_for_each_bio(bio, rq) {
+		seq_printf(m, ", .bio = %px", bio);
+		seq_printf(m, ", .sector = %llu, .len=%u",
+			   bio->bi_iter.bi_sector, bio->bi_iter.bi_size);
+		seq_puts(m, ", .bio_pages = { ");
+		bio_for_each_segment_all(bvec, bio, iter_all) {
+			struct page *page = bvec->bv_page;
+
+			if (!page)
+				continue;
+			seq_printf(m, "%px ", page);
+		}
+		seq_puts(m, "}");
+	}
+	if (mq_ops->show_rq)
+		mq_ops->show_rq(m, rq);
+	seq_puts(m, "}\n");
+}
 
 static void *hctx_dispatch_start(struct seq_file *m, loff_t *pos)
 	__acquires(&hctx->lock)

block/blk-sysfs.c
@@ -553,6 +553,27 @@ static ssize_t queue_io_timeout_store(struct gendisk *disk, const char *page,
 	return count;
 }
 
+static ssize_t queue_hang_threshold_show(struct gendisk *disk, char *page)
+{
+	return sysfs_emit(page, "%u\n", READ_ONCE(disk->queue->rq_hang_threshold));
+}
+
+static ssize_t queue_hang_threshold_store(struct gendisk *disk, const char *page,
+				size_t count)
+{
+	unsigned int hang_threshold;
+	int err;
+	struct request_queue *q = disk->queue;
+
+	err = kstrtou32(page, 10, &hang_threshold);
+	if (err || hang_threshold == 0)
+		return -EINVAL;
+
+	blk_queue_rq_hang_threshold(q, hang_threshold);
+
+	return count;
+}
+
 static ssize_t queue_wc_show(struct gendisk *disk, char *page)
 {
 	if (blk_queue_write_cache(disk->queue))
@@ -663,6 +684,7 @@ QUEUE_LIM_RO_ENTRY(queue_dax, "dax");
 QUEUE_RW_ENTRY(queue_io_timeout, "io_timeout");
 QUEUE_LIM_RO_ENTRY(queue_virt_boundary_mask, "virt_boundary_mask");
 QUEUE_LIM_RO_ENTRY(queue_dma_alignment, "dma_alignment");
+QUEUE_RW_ENTRY(queue_hang_threshold, "hang_threshold");
 
 /* legacy alias for logical_block_size: */
 static const struct queue_sysfs_entry queue_hw_sector_size_entry = {
@@ -775,6 +797,7 @@ static const struct attribute *const queue_attrs[] = {
 	&queue_virt_boundary_mask_entry.attr,
 	&queue_dma_alignment_entry.attr,
 	&queue_ra_entry.attr,
+	&queue_hang_threshold_entry.attr,
 
 	/*
 	 * Attributes which don't require locking.

block/genhd.c
@@ -1120,6 +1120,23 @@ ssize_t part_inflight_show(struct device *dev, struct device_attribute *attr,
 	return sysfs_emit(buf, "%8u %8u\n", inflight[READ], inflight[WRITE]);
 }
 
+ssize_t part_hang_show(struct device *dev, struct device_attribute *attr,
+		       char *buf)
+{
+	struct block_device *bdev = dev_to_bdev(dev);
+	struct request_queue *q = bdev_get_queue(bdev);
+	unsigned int hang[2] = {0, 0};
+
+	/*
+	 * For now, we only support mq device, since don't find a generic method
+	 * to track reqs in single queue device.
+	 */
+	if (queue_is_mq(q))
+		blk_mq_hang_rw(q, bdev, hang);
+
+	return sysfs_emit(buf, "%8u %8u\n", hang[0], hang[1]);
+}
+
 static ssize_t disk_capability_show(struct device *dev,
 				    struct device_attribute *attr, char *buf)
 {
@@ -1170,6 +1187,7 @@ static DEVICE_ATTR(discard_alignment, 0444, disk_discard_alignment_show, NULL);
 static DEVICE_ATTR(capability, 0444, disk_capability_show, NULL);
 static DEVICE_ATTR(stat, 0444, part_stat_show, NULL);
 static DEVICE_ATTR(inflight, 0444, part_inflight_show, NULL);
+static DEVICE_ATTR(hang, 0444, part_hang_show, NULL);
 static DEVICE_ATTR(badblocks, 0644, disk_badblocks_show, disk_badblocks_store);
 static DEVICE_ATTR(diskseq, 0444, diskseq_show, NULL);
 static DEVICE_ATTR(partscan, 0444, partscan_show, NULL);
@@ -1218,6 +1236,7 @@ static struct attribute *disk_attrs[] = {
 	&dev_attr_capability.attr,
 	&dev_attr_stat.attr,
 	&dev_attr_inflight.attr,
+	&dev_attr_hang.attr,
 	&dev_attr_badblocks.attr,
 	&dev_attr_events.attr,
 	&dev_attr_events_async.attr,